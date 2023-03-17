5b3034bf55a411fbba4e654d98c4b697be227e54
Daniil Konovalov <dvkonovalov@miem.hse.ru>
2023-03-17 09:10:47 +0300

    test bin file

---
+++ proba.py
@@ -0,0 +1,19 @@
+import os
+from array import *
+
+
+file = open('test.bin', 'wb')
+data = open('file.jpeg2000', 'r')
+for i in range(5):
+    bitstring = data.readline()
+
+for i in range(1):
+    bitstring = data.readline()
+    print(len(bitstring))
+    splits = [bitstring[x:x + 8] for x in range(0, len(bitstring)-8, 8)]
+
+    bin_array_in = array('B')
+    for split in splits:
+        bin_array_in.append(int(split, 2))
+    bin_array_in.tofile(file)
+
